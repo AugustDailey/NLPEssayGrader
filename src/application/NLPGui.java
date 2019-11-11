aea4bde76a9eac817bbab703e198e3f3688a5230
August B Dailey <daileyab@rose-hulman.edu>
2019-11-11 14:37:50 -0500

    Add Research Paper and Submission Summary Gui Components

--- src/application/NLPGui.java
+++ src/application/NLPGui.java
@@ -2,13 +2,24 @@ package application;
 
 import java.awt.Dimension;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class NLPGui {
 	
 	public JFrame frame;
 	public JPanel panel;
+	
+	public JLabel paperLabel;
+	public JTextField paperPath;
+	public JButton paperButton;
+	
+	public JLabel summaryLabel;
+	public JTextField summaryPath;
+	public JButton summaryButton;
 	
 	
 	public NLPGui() {
@@ -18,12 +29,54 @@ public class NLPGui {
 	private void initialize() {
 		constructJFrame();
 		constructJPanel();
-		frame.add(panel);
+		constructJTextFieldHeaders();
+		constructJTextFields();
+		constructJButtons();
+		addComponentsToFrame();	
 		finalizeFrame();
-		
-		
 	}
 	
+	private void addComponentsToFrame() {
+		frame.add(paperLabel);
+		frame.add(paperPath);
+		frame.add(paperButton);
+		
+		frame.add(summaryLabel);
+		frame.add(summaryPath);
+		frame.add(summaryButton);	
+	}
+
+	private void constructJTextFieldHeaders() {
+		paperLabel = new JLabel();
+		paperLabel.setText("Research Paper");
+	    paperLabel.setBounds(50, 70, 200, 30); 
+	    
+	    summaryLabel = new JLabel();
+	    summaryLabel.setText("Student Summary Submission");
+	    summaryLabel.setBounds(50, 170, 200, 30);
+		
+	}
+
+	private void constructJButtons() {
+		paperButton = new JButton();
+		paperButton.setText("Browse...");
+		paperButton.setBounds(275, 100, 100, 30);
+		
+		summaryButton = new JButton();
+		summaryButton.setText("Browse...");
+		summaryButton.setBounds(275, 200, 100, 30);
+		
+	}
+
+	private void constructJTextFields() {
+		paperPath = new JTextField("--Research Paper Path--");  
+	    paperPath.setBounds(50, 100, 200, 30);  
+	    
+	    summaryPath = new JTextField("--Summary Path--");  
+	    summaryPath.setBounds(50, 200, 200, 30);  
+		
+	}
+
 	private void constructJPanel() {
 		panel = new JPanel();
 		panel.setSize(new Dimension(500, 500));
@@ -41,6 +94,7 @@ public class NLPGui {
 	
 	private void finalizeFrame() {
 		//frame.pack();
+		frame.setLayout(null);
 		frame.setVisible(true);
 	}
 
